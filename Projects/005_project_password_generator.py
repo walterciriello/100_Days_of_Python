d9cf8df303d030569896b276982d24a5bd5b939f
Walter Ciriello <106283495+walterciriello@users.noreply.github.com>
2022-05-27 18:50:52 -0300

    update

--- Projects/005_project_password_generator.py
+++ Projects/005_project_password_generator.py
@@ -1,17 +1,19 @@
 # Password Generator
 
+# importing module and creating lists
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+print("Welcome to the Password Generator!")
 
-# Usando loop para adicionar o número de itens escolhidos em cada lista
-# escolhas feitas de forma aleatória utilizando random.choice()
+# recording user choices regarding the number of characters
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+# creating empty list and adding random characters
 password = []
 for number in range(0, nr_letters):
     password.append(random.choice(letters))
@@ -20,7 +22,6 @@ for number in range(0, nr_symbols):
 for number in range(0, nr_numbers):
     password.append(random.choice(numbers))
 
-print(password) # lista criada originalmente
-random.shuffle(password) # random.shuffle() embaralha a sequência inicial
-print(password)
-print("".join(password)) # .join() concatena as strings em uma só
+# scrambling password and joining into a single string
+random.shuffle(password)
+print("".join(password))
