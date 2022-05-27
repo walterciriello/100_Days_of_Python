ec51f37be60b8c64ab0bba46dec582c486f86d77
Walter Ciriello <106283495+walterciriello@users.noreply.github.com>
2022-05-27 18:41:16 -0300

    update

--- Projects/004_project_rock_paper_scissors.py
+++ Projects/004_project_rock_paper_scissors.py
@@ -1,13 +1,18 @@
 #Game rock, paper, scissors
+
+# importing module "random"
 import random
 
+# choosing random number in a defined range "random.randint()"
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
 computer_choice = random.randint(0, 2)
 list = ["ROCK", "PAPER", "SCISSORS"]
 
+# printing and relating the choices to the list
 print(f"You chose {list[user_choice]}!")
 print(f"Computer chose {list[computer_choice]}!")
 
+# using conditionals to get the result
 if user_choice == 0:
     if computer_choice == 0:
         print("You tied!")
