289558ad8e6d40bd3d51b9d61fe15f7ef01ea612
Walter Ciriello <106283495+walterciriello@users.noreply.github.com>
2022-05-27 16:57:47 -0300

    Update 001_project_band_name_generator.py

--- Projects/001_project_band_name_generator.py
+++ Projects/001_project_band_name_generator.py
@@ -1,4 +1,11 @@
+# Band Generator
+# criando nome de uma banda através da junção de duas strings
+
 print("Welcome to the Band Name Generator!")
+
+# atribuindo valores a uma variável através do "input"
 city = input("What's name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
+
+# concatenando strings utilizando "+"
 print("Your band name could be " + city + " " + pet)
