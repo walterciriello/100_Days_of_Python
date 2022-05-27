336d15cd187d0405ef6b58265ef450cb9f8e79a7
Walter Ciriello <106283495+walterciriello@users.noreply.github.com>
2022-05-27 20:13:49 -0300

    update

---
+++ Exercise/005.4_exercise_fizz_buzz.py
@@ -0,0 +1,12 @@
+# FizzBuzz
+
+# using loop with defined range and conditionals
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fiz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
